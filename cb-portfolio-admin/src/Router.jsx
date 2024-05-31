6123c5de8a0edb09d11dfab2a598c2bab7890dce
Chetan Bedakihale <chetan.bedakihale80@gmail.com>
2024-05-31 14:11:39 +0530

    ADDED: Add component

--- cb-portfolio-admin/src/Router.jsx
+++ cb-portfolio-admin/src/Router.jsx
@@ -1,12 +1,15 @@
 import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./Components/Home/Home";
+import Add from "./Components/Add/Add";
 
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path='/' element = {<Layout />}>
             <Route index element = {<Home />} />
+            <Route path="add" element = {<Add />} />
+            <Route path="update" element = {<Add />} />
         </Route>
     )
 )
